23eea57684c460ac756dc182d0eb575efa9b8b39
Wang <wghappytin@hotmail.com>
2023-08-01 13:27:15 -0400

    update drupal core 9.5.10 and simple_oauth 6beta; setup local DB connenction and others.

--- drupal/web/sites/default/settings.php
+++ drupal/web/sites/default/settings.php
@@ -11,3 +11,20 @@ if (file_exists($app_root . '/' . $site_path . '/settings.ddev.php') && getenv('
 
 $settings['reverse_proxy'] = TRUE;
 $settings['reverse_proxy_addresses'] = array($_SERVER['REMOTE_ADDR']);
+
+$databases['default']['default'] = array (
+  'database' => 'react_drupal',
+  'username' => 'root',
+  'password' => 'gungwang',
+  'prefix' => '',
+  'host' => 'localhost',
+  'port' => '3306',
+  'namespace' => 'Drupal\\Core\\Database\\Driver\\mysql',
+  'driver' => 'mysql',
+);
+
+$settings['hash_salt'] = "GungWang-test-react-drupal-denmo";
+
+$settings['trusted_host_patterns'] = [
+  '^react\.test$',
+];
